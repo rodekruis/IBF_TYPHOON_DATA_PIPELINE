c5df57fdc4bc2549bc53c3c47914426513d4b11d
aklilu <akliludin@gmail.com>
2022-09-23 17:25:37 +0200

    no message

--- IBF-Typhoon-model/src/typhoonmodel/utility_fun/settings.py
+++ IBF-Typhoon-model/src/typhoonmodel/utility_fun/settings.py
@@ -91,7 +91,7 @@ High_resoluation_only_Switch=True
 #ecmwf_remote_directory=None#(start_time - timedelta(hours=10)).strftime("%Y%m%d000000")#None#'20220714120000'
 typhoon_event_name=None
 
-Active_Typhoon_event_list=['NORU']#'MA-ON']
+Active_Typhoon_event_list=[]#'MA-ON']
 WIND_SPEED_THRESHOLD=20
 
 
