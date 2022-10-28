b23df9cd4a9a061f89e4ad8963d35d6aa29413fb
aklilu <akliludin@gmail.com>
2022-10-28 17:18:05 +0200

    no message

--- IBF-Typhoon-model/src/typhoonmodel/utility_fun/secrets.py
+++ IBF-Typhoon-model/src/typhoonmodel/utility_fun/secrets.py
@@ -3,21 +3,4 @@
 ##################
 
 
-IBF_API_URL = 'https://ibf-test.510.global/api/'
 
-DATALAKE_STORAGE_ACCOUNT_NAME = '510datalakestorage'
-DATALAKE_STORAGE_ACCOUNT_KEY = 'RnPSnWqM6sDvTMxMyYVu9cKhqN1yJ6Q5PcqC6WEIzXBtyLOojaoLt71e4VhMfohDTE2NMZ2fwJ6hxT69JD6Fig=='
-DATALAKE_API_VERSION = '2018-11-09'
-DATALAKE_STORAGE_ACCOUNT_KEY_IBFSYSTEM='si=typhoondata&spr=https&sv=2021-06-08&sr=c&sig=Y6fGrFSuVj6UHCXK7FzFe2eg8Xc83uf1ES7nLRJwhEs%3D'
-
-
-#############
-## SECRETS ##
-#############
-
-
-# API-service access
-ADMIN_LOGIN = 'dunant@redcross.nl'
-ADMIN_PASSWORD='password'
-
- 
